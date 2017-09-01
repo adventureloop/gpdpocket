3eedce9e91a6c993a500bd87622bf1646c8c15d8
Tom Jones <jones@sdf.org>
2017-09-01 13:40:31 +0100

    add script to build modules and launch in correct order

---
+++ chv-power/launch.sh
@@ -0,0 +1,18 @@
+#!/bin/sh                                               
+                                                        
+set -e                                                  
+                                                        
+echo "building kernel modules"                          
+echo "  - building max170xx driver"                     
+
+cd ../max170xx
+make                                                    
+cd -
+                                                        
+echo "- building chv pwr driver"                       
+
+make
+
+echo "loading kernel modules"                           
+sudo kldload ../max170xx/max170xx.ko
+sudo kldload ./chv-power.ko
