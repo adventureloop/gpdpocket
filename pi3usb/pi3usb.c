8650f21fb3a89bddab776d0a9de933f29c4ab3eb
Tom Jones <jones@sdf.org>
2017-09-02 16:54:28 +0100

    ignore pi3usb build stuff

---
+++ pi3usb/pi3usb.c
@@ -0,0 +1,161 @@
+/*-
+ * Copyright (c) 2017 Tom Jones <tj@enoti.me>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ */
+
+#include <sys/param.h>
+#include <sys/systm.h>
+#include <sys/bus.h>
+#include <sys/clock.h>
+#include <sys/kernel.h>
+#include <sys/module.h>
+#include <sys/endian.h>
+#include <sys/rman.h>
+
+#include <machine/bus.h>
+#include <machine/resource.h>
+
+#include <dev/iicbus/iicbus.h>
+#include <dev/iicbus/iiconf.h>
+
+struct pi3usb_softc {
+	device_t			sc_dev;
+	uint8_t				sc_addr;
+};
+
+#define PI3USB_SADDR	0x54 // is what my notes say. datasheet is different
+
+static int pi3usb_probe(device_t);
+static int pi3usb_attach(device_t);
+static int pi3usb_detach(device_t);
+static int pi3usb_read(device_t, uint8_t, uint8_t *);
+static int pi3usb_write(device_t, uint8_t, uint8_t );
+
+static int
+pi3usb_probe(device_t dev)
+{
+	device_printf(dev, "probe\n");
+	device_set_desc(dev, "pi3usb USB Type-C Mux");
+	return (0);
+}
+
+static int
+pi3usb_attach(device_t dev)
+{
+	device_printf(dev, "attach\n");
+	struct pi3usb_softc *sc = device_get_softc(dev);
+
+	sc->sc_dev = dev;
+	sc->sc_addr = PI3USB_SADDR;
+
+	return (0);
+}
+
+static int
+pi3usb_detach(device_t dev)
+{
+	return (0);
+}
+
+static int 
+pi3usb_read(device_t dev, uint8_t reg, uint8_t *val)
+{
+	struct pi3usb_softc *sc;
+	struct iic_msg msg[2];
+
+	sc = device_get_softc(dev);
+
+	msg[0].slave = sc->sc_addr;
+	msg[0].flags = IIC_M_WR | IIC_M_NOSTOP;
+	msg[0].len = 1;
+	msg[0].buf = &reg;
+
+	msg[1].slave = sc->sc_addr;
+	msg[1].flags = IIC_M_RD;
+	msg[1].len = sizeof(uint8_t);
+	msg[1].buf = val;
+
+	return (iicbus_transfer(dev, msg, 2));
+}
+
+static int 
+pi3usb_write(device_t dev, uint8_t reg, uint8_t val)
+{
+	struct pi3usb_softc *sc;
+	struct iic_msg msg[2];
+
+	sc = device_get_softc(dev);
+	reg = htobe16(reg);
+
+	msg[0].slave = sc->sc_addr;
+	msg[0].flags = IIC_M_WR | IIC_M_NOSTOP;
+	msg[0].len = 1;
+	msg[0].buf = &reg;
+
+	msg[1].slave = sc->sc_addr;		
+	msg[1].flags = IIC_M_WR;
+	msg[1].len = sizeof(uint8_t);
+	msg[1].buf = &val;
+
+	return (iicbus_transfer(dev, msg, 2));
+}
+
+static int
+pi3usb_driver_loaded(struct module *m, int what, void *arg)
+{
+	int err = 0;
+
+	switch (what) {
+	case MOD_LOAD:
+		uprintf("pi3usb KLD loaded.\n");
+		break;
+	case MOD_UNLOAD:
+		uprintf("pi3usb KLD unloaded.\n");
+		break;
+	default:
+		err = EOPNOTSUPP;
+		break;
+	}
+	return(err);
+}
+
+static device_method_t pi3usb_methods[] = {
+	DEVMETHOD(device_probe,		pi3usb_probe),
+	DEVMETHOD(device_attach,	pi3usb_attach),
+	DEVMETHOD(device_detach,	pi3usb_detach),
+	DEVMETHOD_END
+};
+
+static driver_t pi3usb_driver = {
+	.name = "pi3usb",
+	.methods = pi3usb_methods,
+	.size = sizeof(struct pi3usb_softc)
+};
+
+static devclass_t pi3usb_devclass;
+DRIVER_MODULE(pi3usb, iicbus, pi3usb_driver, pi3usb_devclass, pi3usb_driver_loaded, NULL);
+
+MODULE_DEPEND(pi3usb, iicbus, IICBUS_MINVER, IICBUS_PREFVER, IICBUS_MAXVER);
+MODULE_VERSION(pi3usb, 1);
