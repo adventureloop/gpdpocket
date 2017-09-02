edeed8fe657a41c88e8b2cecb397e6e6c8b1b76d
Tom Jones <jones@sdf.org>
2017-09-02 14:31:44 +0100

    force bus probe and attach, clean up children again

--- chv-power/chv-power.c
+++ chv-power/chv-power.c
@@ -140,6 +140,7 @@ chvpower_attach(device_t dev)
 		if (child != NULL) {
 			iicbus_set_addr(child, sc->sc_iicchildren[1].address);
 			sc->sc_max170xx = child;
+			bus_generic_attach(iicbus);
 			device_printf(dev, "added child\n");
 		} else
 			device_printf(dev, "failed to add child\n");
@@ -292,9 +293,9 @@ chvpower_detach(device_t dev)
 	int child; 
 	struct chvpower_softc *sc;
 	sc = device_get_softc(dev);
-/*
+
 	device_delete_child(device_get_parent(sc->sc_max170xx), sc->sc_max170xx);
-*/
+
 	for (child = 0; child < IIC_CHILD_MAX; child++) {
 		if (child == 1)		//HACK TODO REMOVE
 			continue;
