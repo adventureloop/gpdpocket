aa5ac6f7fc3008c4240699aec8e7ab16e90af6d3
Tom Jones <jones@sdf.org>
2017-09-02 20:08:00 +0100

    build and launch pi3usb

--- launch.sh
+++ launch.sh
@@ -3,17 +3,24 @@
 set -e                                                  
                                                         
 echo "building kernel modules"                          
+echo "  - building pi3usb driver"                     
+
+cd pi3usb
+make                                                    
+cd -
+                                                        
+echo "  - building fusb3 driver"                     
+
+cd fusb3
+make                                                    
+cd -
+
 echo "  - building max170xx driver"                     
 
 cd max170xx
 make                                                    
 cd -
 
-echo "  - building fusb3 driver"                     
-
-cd fusb3
-make                                                    
-cd -
                                                         
 echo "- building chv pwr driver"                       
 
@@ -24,8 +31,10 @@ cd -
 echo "loading kernel modules"                           
 sudo kldload ./max170xx/max170xx.ko
 sudo kldload ./fusb3/fusb3.ko
+sudo kldload ./pi3usb/pi3usb.ko
 sudo kldload ./chv-power/chv-power.ko
 
 sudo kldunload chv-power.ko
+sudo kldunload pi3usb.ko
 sudo kldunload fusb3.ko
 sudo kldunload max170xx.ko
