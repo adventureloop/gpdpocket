7a3bb8e355834affa271cf125a2a56f059569c51
Tom Jones <jones@sdf.org>
2017-09-02 22:37:46 +0100

    parse the crs

--- chvgpio/chvgpio.c
+++ chvgpio/chvgpio.c
@@ -52,47 +52,47 @@
 #define IIC_CHILD_MAX 4
 #define DEBUG 0
 
-static MALLOC_DEFINE(M_CHVPWR, "chv-power", "CHV Power Driver");
+static MALLOC_DEFINE(M_CHVGPIO, "chvgpio", "CHV GPIO Driver");
 
 
-struct chvpower_child {
+struct chvgpio_child {
 	uint8_t address;
 	char *resource_source;
 };
 
-struct chvpower_softc {
+struct chvgpio_softc {
 	device_t				sc_dev;
 	ACPI_HANDLE				sc_handle;
 };
 
-static char *chvpower_hids[] = {
+static char *chvgpio_hids[] = {
 	"INT33FF",     
 	NULL            
 };                  
 
-static int chvpower_probe(device_t);
-static int chvpower_attach(device_t);
-static int chvpower_detach(device_t);
+static int chvgpio_probe(device_t);
+static int chvgpio_attach(device_t);
+static int chvgpio_detach(device_t);
 
 static ACPI_STATUS acpi_collect_gpio(ACPI_RESOURCE *, void *);
 
 static int
-chvpower_probe(device_t dev)
+chvgpio_probe(device_t dev)
 {
-	if (acpi_disabled("chvpower") ||
-    ACPI_ID_PROBE(device_get_parent(dev), dev, chvpower_hids) == NULL)
+	if (acpi_disabled("chvgpio") ||
+    ACPI_ID_PROBE(device_get_parent(dev), dev, chvgpio_hids) == NULL)
 		return (ENXIO);
 
-	device_set_desc(dev, "Intel Cherry View Power Nexus");
+	device_set_desc(dev, "Intel Cherry View GPIO");
 	return (0);
 }
 
 static int
-chvpower_attach(device_t dev)
+chvgpio_attach(device_t dev)
 {
-	device_printf(dev, "chvpower attach\n");
+	device_printf(dev, "chvgpio attach\n");
 
-	struct chvpower_softc *sc;
+	struct chvgpio_softc *sc;
 	ACPI_STATUS status;
 	int uid;
 
@@ -102,11 +102,9 @@ chvpower_attach(device_t dev)
 	status = acpi_GetInteger(sc->sc_handle, "_UID", &uid);
 	if (ACPI_FAILURE(status)) {
 		device_printf(dev, "failed to read _UID\n");
-		return (ENXIO);
+//		return (ENXIO);
 	} else 
 		device_printf(dev, "_UID %d\n", uid);
-
-	return ENXIO;
 
 	sc->sc_handle = acpi_get_handle(dev);
 	status = AcpiWalkResources(sc->sc_handle, "_CRS", 
@@ -121,13 +119,12 @@ acpi_collect_gpio(ACPI_RESOURCE *res, void *context)
 	int type;
 	struct link_count_request *req;
 	device_t dev = (device_t)context;
-	struct chvpower_softc *sc;
+	struct chvgpio_softc *sc;
 	sc = device_get_softc(dev);
 
 	req = (struct link_count_request *)context;
-#if DEBUG
 	device_printf(dev, "resource of number: %x\n", res->Type);
-#endif
+
 	switch (res->Type) {
 	case ACPI_RESOURCE_TYPE_SERIAL_BUS:
 #if DEBUG
@@ -205,25 +202,25 @@ acpi_collect_gpio(ACPI_RESOURCE *res, void *context)
 }
 
 static int
-chvpower_detach(device_t dev)
+chvgpio_detach(device_t dev)
 {
-	struct chvpower_softc *sc;
+	struct chvgpio_softc *sc;
 	sc = device_get_softc(dev);
 
 	return (0);
 }
 
 static int
-chvpower_driver_loaded(struct module *m, int what, void *arg)
+chvgpio_driver_loaded(struct module *m, int what, void *arg)
 {
 	int err = 0;
 
 	switch (what) {
 	case MOD_LOAD:
-		uprintf("chvpower KLD loaded.\n");
+		uprintf("chvgpio KLD loaded.\n");
 		break;
 	case MOD_UNLOAD:
-		uprintf("chvpower KLD unloaded.\n");
+		uprintf("chvgpio KLD unloaded.\n");
 		break;
 	default:
 		err = EOPNOTSUPP;
@@ -232,21 +229,21 @@ chvpower_driver_loaded(struct module *m, int what, void *arg)
 	return(err);
 }
 
-static device_method_t chvpower_methods[] = {
-	DEVMETHOD(device_probe,		chvpower_probe),
-	DEVMETHOD(device_attach,	chvpower_attach),
-	DEVMETHOD(device_detach,	chvpower_detach),
+static device_method_t chvgpio_methods[] = {
+	DEVMETHOD(device_probe,		chvgpio_probe),
+	DEVMETHOD(device_attach,	chvgpio_attach),
+	DEVMETHOD(device_detach,	chvgpio_detach),
 	DEVMETHOD_END
 };
 
-static driver_t chvpower_driver = {
-	.name = "chvpower",
-	.methods = chvpower_methods,
-	.size = sizeof(struct chvpower_softc)
+static driver_t chvgpio_driver = {
+	.name = "chvgpio",
+	.methods = chvgpio_methods,
+	.size = sizeof(struct chvgpio_softc)
 };
 
-static devclass_t chvpower_devclass;
-DRIVER_MODULE(chvpower, acpi, chvpower_driver, chvpower_devclass, chvpower_driver_loaded, NULL);
+static devclass_t chvgpio_devclass;
+DRIVER_MODULE(chvgpio, acpi, chvgpio_driver, chvgpio_devclass, chvgpio_driver_loaded, NULL);
 
-MODULE_DEPEND(chvpower, acpi, 1, 1, 1);
-MODULE_VERSION(chvpower, 1);
+MODULE_DEPEND(chvgpio, acpi, 1, 1, 1);
+MODULE_VERSION(chvgpio, 1);
