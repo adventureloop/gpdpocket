b8f93d9e26ec794494396dbbb7c0f0605605472d
Tom Jones <jones@sdf.org>
2017-09-02 16:38:52 +0100

    build and run fusb3 too

--- launch.sh
+++ launch.sh
@@ -5,18 +5,27 @@ set -e
 echo "building kernel modules"                          
 echo "  - building max170xx driver"                     
 
-cd ../max170xx
+cd max170xx
+make                                                    
+cd -
+
+echo "  - building fusb3 driver"                     
+
+cd fusb3
 make                                                    
 cd -
                                                         
 echo "- building chv pwr driver"                       
 
+cd chv-power
 make
+cd -
 
 echo "loading kernel modules"                           
-sudo kldload ../max170xx/max170xx.ko
-sudo kldload ./chv-power.ko
+sudo kldload ./max170xx/max170xx.ko
+sudo kldload ./fusb3/fusb3.ko
+sudo kldload ./chv-power/chv-power.ko
 
-
-sudo kldunload max170xx.ko
 sudo kldunload chv-power.ko
+sudo kldunload fusb3.ko
+sudo kldunload max170xx.ko
