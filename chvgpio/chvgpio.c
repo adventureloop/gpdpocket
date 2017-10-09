333f85275e52b16a09a0d22fcb699eb18e8872c9
Tom Jones <jones@sdf.org>
2017-10-09 13:15:44 +0100

    log pin number checks

--- chvgpio/chvgpio.c
+++ chvgpio/chvgpio.c
@@ -202,6 +202,7 @@ chvgpio_pin_max(device_t dev, int *maxpin)
 static int
 chvgpio_valid_pin(struct chvgpio_softc *sc, int pin)
 {
+	device_printf(sc->sc_dev, "checking pin %d", pin);
 	if (pin < 0)
 		return EINVAL;
 	if ((pin / 15) >= sc->sc_ngroups)
